67f12c9c1090ac4b2179c639b4b44c40cb17770e
Mohammad <malnajjar16@gmail.com>
2021-09-15 12:04:26 +0200

    register done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,34 @@
 const User = require('../models/Users');
 const bcrypt = require('bcrypt');
-exports.registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
 
-  res.send('register user');
+exports.registerUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  const salt = 10;
+  try {
+    const hash = await bcrypt.hash(password, salt);
+    const registeredUser = await User.create({
+      firstName,
+      lastName,
+      email,
+      hash,
+    });
+    res.json({
+      firstName,
+      lastName,
+      email,
+      joinedAt: registeredUser.joinedAt,
+      status: {
+        register: 'Success registered',
+      },
+    });
+  } catch (error) {
+    res.json({
+      msg: error,
+      status: {
+        register: 'Failed to register',
+      },
+    });
+  }
 };
 exports.loginUser = async (req, res) => {
   res.send('login user');
