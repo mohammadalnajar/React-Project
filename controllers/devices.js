4f01926ee8dcc19b8d23473d905aa16356af5c8e
Mohammad <malnajjar16@gmail.com>
2021-09-15 15:02:02 +0200

    add device error handling

--- controllers/devices.js
+++ controllers/devices.js
@@ -3,9 +3,21 @@ const Device = require('../models/Device');
 exports.addDevice = async (req, res) => {
   try {
     const device = await Device.create(req.body);
-    res.json(device);
+    res.json({
+      device,
+      status: {
+        msg: 'Device is successfully added to db',
+        added: true,
+      },
+    });
   } catch (error) {
-    console.log(error);
+    res.status(500).json({
+      status: {
+        msg: 'Failed to add device',
+        error,
+        added: false,
+      },
+    });
   }
 };
 exports.getDevices = async (req, res) => {
