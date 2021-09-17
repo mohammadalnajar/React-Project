9057faa8b60380f7774bb69b76a6124e58d1f2a8
Mohammad <malnajjar16@gmail.com>
2021-09-17 12:27:15 +0200

    save user in localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import { RegisterPage } from './pages/RegisterPage';
-import { GlobalProvider } from './context/global/GlobalContext';
 import { LoginPage } from './pages/LoginPage';
+import { useContext, useEffect } from 'react';
+import { GlobalContext } from './context/global/GlobalContext';
 function App() {
+  const { setUser } = useContext(GlobalContext);
+
+  // get user info from localStorage
+  useEffect(() => {
+    const loggedInUserInfo = localStorage.getItem('user');
+    if (loggedInUserInfo) {
+      const userInfo = JSON.parse(loggedInUserInfo);
+      setUser(userInfo);
+    }
+  }, []);
   return (
     <>
-      <GlobalProvider>
-        <Switch>
-          <Route exact path='/' component={RegisterPage} />
-          <Route exact path='/login' component={LoginPage} />
-        </Switch>
-      </GlobalProvider>
+      <Switch>
+        <Route exact path='/' component={RegisterPage} />
+        <Route exact path='/login' component={LoginPage} />
+      </Switch>
     </>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalProvider } from './context/global/GlobalContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <GlobalProvider>
+      <Router>
+        <App />
+      </Router>
+    </GlobalProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
