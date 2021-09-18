a92f797e68cc44dd8bac012eb0a3875f6f3cb55b
Mohammad <malnajjar16@gmail.com>
2021-09-18 11:49:51 +0200

    device item with material ui

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@mui/styles';
 import notFound from '../assets/not-found-img.jpg';
+import iphone13proImage from '../assets/iphone13pro.png';
 import { AddToCart } from './AddToCart';
+import { Grid } from '@material-ui/core';
 
 export const DeviceItem = ({ device }) => {
   const useStyles = makeStyles({
@@ -24,19 +26,24 @@ export const DeviceItem = ({ device }) => {
   });
   const classes = useStyles();
   return (
-    <div className={classes.deviceContainer}>
-      <ul style={{ listStyle: 'none' }}>
-        <li>
-          <img src={notFound} alt='not found' width='50px' />
-        </li>
-        <li>
-          {device.name} {device.specs.memory.storage[0]}
-        </li>
-        <li>${device.price}-</li>
-        <li>
+    <Grid item xs={3} className={`${classes.deviceContainer} m-1`}>
+      <Grid container>
+        <Grid item xs={12} container justifyContent='center'>
+          <img src={iphone13proImage} alt='not found' width='100px' />
+        </Grid>
+        <Grid justifyContent='center' container item>
+          <Grid justifyContent='center' container item xs={8}>
+            {device.name}
+          </Grid>
+          <Grid item xs={4}>
+            {device.specs.memory.storage[0]}
+          </Grid>
+          <Grid item>${device.price}-</Grid>
+        </Grid>
+        <Grid justifyContent='flex-end' className={'m-2'} container item>
           <AddToCart />
-        </li>
-      </ul>
-    </div>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
--- client/src/pages/ShopPage.js
+++ client/src/pages/ShopPage.js
@@ -4,7 +4,7 @@ import { Grid } from '@material-ui/core';
 
 export const ShopPage = ({ devices }) => {
   return (
-    <Grid container>
+    <Grid container justifyContent='flex-end'>
       {devices.length > 1 &&
         devices.map((device) => {
           return <DeviceItem key={device._id} device={device} />;
